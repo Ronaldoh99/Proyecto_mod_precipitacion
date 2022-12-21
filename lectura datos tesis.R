25cecfcd74dff04f457e1fdbc635d4be074ff8a6
velasco87 <hector.velasco@correounivalle.edu.co>
2022-12-20 23:17:34 -0500

    bases de datos candidatas

---
+++ lectura datos tesis.R
@@ -0,0 +1,6 @@
+## este script es para leer los datos 
+load("~/semestre 9/trabajo de grado/Proyecto_mod_precipitacion/Datos/wth_data_caf_ideam.RData")
+
+##bases candidatas
+View(ws_selected[[3]][[301]])
+
