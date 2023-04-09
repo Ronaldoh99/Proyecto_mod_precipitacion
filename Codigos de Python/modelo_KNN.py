5dc6091072d8478fbd28557d977160f834d50311
Ronaldo <ronaldo.hernandez@correounivalle.edu.co>
2023-04-08 20:58:39 -0500

    ajuste al modelo KNN

--- Codigos de Python/modelo_KNN.py
+++ Codigos de Python/modelo_KNN.py
@@ -68,6 +68,8 @@ show(datos)
 #  para la imputación de datos faltantes utilizando KNN y otras técnicas.
 datos.columns
 
+import pandas as pd
+import numpy as np
 from sklearn.impute import KNNImputer
 
 def impute_precipitation_for_station(station_data, n_neighbors=5):
@@ -76,14 +78,23 @@ def impute_precipitation_for_station(station_data, n_neighbors=5):
     station_data['precipitation_imputed'] = station_data_imputed
     return station_data
 
+# Crear datos de ejemplo
+data = pd.DataFrame({
+    'station_id': [1, 1, 2, 2, 3, 3],
+    'precipitation': [1, np.nan, 3, 4, np.nan, 6]
+})
 
-stations = datos['ID'].unique()
+stations = data['station_id'].unique()
 imputed_data = pd.DataFrame()
 
 for station in stations:
-    station_data = datos[datos['ID'] == station]
+    station_data = data[data['station_id'] == station]
     station_data_imputed = impute_precipitation_for_station(station_data)
     imputed_data = imputed_data.append(station_data_imputed)
+
+print(imputed_data)
+
+####Fin de prueba con pocos datos
 
 # Crear un nuevo DataFrame con nombres de columnas cambiados
 data = datos.rename(columns={'Date': 'date', 'prec': 'precipitation','ID':'station_id'})
@@ -145,7 +156,7 @@ imputed_data = impute_precipitation_parallel(data)
 print(imputed_data)
 
 
-#parelizacion con gpu 
+#parelizacion con gpu no funciona 
 import cudf
 import cupy as cp
 
@@ -177,7 +188,7 @@ print(imputed_data)
 
 
 
-###prueba
+###prueba no funciona 
 import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
@@ -210,3 +221,106 @@ data = pd.DataFrame({
 
 imputed_data = impute_precipitation_parallel(data)
 print(imputed_data)
+
+
+
+####prueba en paralelo funciona para datos de prueba
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+from joblib import Parallel, delayed
+
+def impute_precipitation_for_station(station_data, n_neighbors=5):
+    imputer = KNNImputer(n_neighbors=n_neighbors)
+    station_data_imputed = imputer.fit_transform(station_data[['prec']])
+    station_data['prec_imputed'] = station_data_imputed
+    return station_data
+
+def impute_precipitation_parallel(data, n_jobs=-1):
+    stations = data['ID'].unique()
+    results = Parallel(n_jobs=n_jobs)(
+        delayed(impute_precipitation_for_station)(data[data['ID'] == station]) for station in stations
+    )
+    imputed_data = pd.concat(results, ignore_index=True)
+    return imputed_data
+
+
+data = pd.read_csv("Datos/datos_combinados_para_modelo_con_coordenadas.txt",sep=";")
+data.info()
+
+#datos de prueba
+data = pd.DataFrame({
+    'Date': pd.date_range(start='2022-01-01', periods=5, freq='D').tolist() * 3,
+    'prec': [1, np.nan, 3, 4, np.nan] * 3,
+    'ID': [1] * 5 + [2] * 5 + [3] * 5,
+    'LAT': [12.34] * 5 + [56.78] * 5 + [90.12] * 5,
+    'LON': [-76.54] * 5 + [123.45] * 5 + [167.89] * 5
+})
+data['Date'] = pd.to_datetime(data['Date']) 
+
+imputed_data = impute_precipitation_parallel(data)
+print(imputed_data)
+imputed_data.info()
+
+
+#####prueba 2.0  funciona con todos los datos 
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+from joblib import Parallel, delayed
+import dask.dataframe as dd
+
+def impute_precipitation_for_station(station_data, n_neighbors=5):
+    imputer = KNNImputer(n_neighbors=n_neighbors)
+    station_data_imputed = imputer.fit_transform(station_data[['prec']])
+    
+    # Crear un nuevo DataFrame en lugar de modificar el existente
+    imputed_df = station_data.copy()
+    imputed_df['prec_imputed'] = station_data_imputed
+    
+    return imputed_df
+
+
+
+def impute_precipitation_parallel(data, npartitions=10):
+    ddata = dd.from_pandas(data, npartitions=npartitions)
+    stations = data['ID'].unique()
+    
+    results = []
+    for station in stations:
+        station_data = ddata[ddata['ID'] == station].compute()
+        
+        # Check if there are enough valid values to perform imputation
+        if station_data['prec'].notna().sum() >= 2:
+            station_data_imputed = impute_precipitation_for_station(station_data)
+            results.append(station_data_imputed)
+        else:
+            results.append(station_data)
+    
+    imputed_data = pd.concat(results, ignore_index=True)
+    return imputed_data
+
+
+
+data = pd.read_csv("Datos/datos_combinados_para_modelo_con_coordenadas.txt",sep=";")
+data.info()
+
+#datos de prueba
+data = pd.DataFrame({
+    'Date': pd.date_range(start='2022-01-01', periods=5, freq='D').tolist() * 3,
+    'prec': [1, np.nan, 3, 4, np.nan] * 3,
+    'ID': [1] * 5 + [2] * 5 + [3] * 5,
+    'LAT': [12.34] * 5 + [56.78] * 5 + [90.12] * 5,
+    'LON': [-76.54] * 5 + [123.45] * 5 + [167.89] * 5
+})
+data['Date'] = pd.to_datetime(data['Date']) 
+
+imputed_data = impute_precipitation_parallel(data)
+print(imputed_data)
+imputed_data.info()
+soloNAn=na_rows = imputed_data[imputed_data['prec'].isna()]
+soloNAn
+
+data.describe['prec']
+summary = data['prec'].describe()
+print(summary)
